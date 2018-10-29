34c8a5c2cd81058184ee416562da98ca880945ae
Akshay <akshayk.vnit@gmail.com>
2018-10-30 02:56:30 +0530

    Added code to flip each image horizontally,  generate modified label file and also add the flipped image path to the filenames list required by YOLO.

--- stair_detection/conv_xml2csv.py
+++ stair_detection/conv_xml2csv.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import os
+import cv2
 
 def parse_xml(path):
     xmin = 0
@@ -47,23 +48,38 @@ def parse_xml(path):
         print(x, y, box_width, box_height)
         # filename[:-4] implies dropping last 4 characters since
         # -4 index means 4th character from the end
-        fullpath = 'test/' + filename[:-4] + ".txt"
+        fullpath = 'train/' + filename[:-4] + ".txt"
+        full_flipped_path = 'train/' + filename[:-4] + "_flipped.txt"
         with open(fullpath, 'a') as text_file:
             # writing in the format - class x y box_width box_height
             label = str(0) + " " + str(x) + " " + str(y) + " " + str(box_width) + " " + str(box_height) + "\n"
             text_file.writelines(label)
 
-    with open('test_filenames.txt', 'a') as text_file:
-        ultrapath = '/home/nvidia/darknet/data/' + 'test/' + filename + '\n'
+        with open(full_flipped_path, 'a') as text_file:
+            label = str(0) + " " + str(1 - x) + " " + str(y) + " " + str(box_width) + " " + str(box_height) + "\n"
+            text_file.writelines(label)
+
+    with open('train_filenames.txt', 'a') as text_file:
+        ultrapath = '/home/nvidia/darknet/data/' + 'train/' + filename + '\n'
         text_file.writelines(ultrapath)
+        ultra_flipped_path = "/home/nvidia/darknet/data/" + 'train/' + filename[:-4] + '_flipped.jpg' + '\n'
+        text_file.writelines(ultra_flipped_path)
 
 def main():
-    path = 'test/'
+    path = 'train/'
     for filename in os.listdir(path):
         if not filename.endswith('.xml'):
             continue
         fullname = os.path.join(path, filename)
         parse_xml(fullname)
+
+    for filename in os.listdir(path):
+        if filename.endswith('.jpg' or '.JPG'):
+            img = cv2.imread(os.path.join(path, filename))
+            img = cv2.flip(img, 1)
+            final_path = filename[:-4] + '_flipped.jpg'
+            print(os.path.join(path, final_path))
+            cv2.imwrite(str(os.path.join(path, final_path)), img)
 
 if __name__ == '__main__':
     main()
